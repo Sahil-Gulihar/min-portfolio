f09ba13ccf679aab87a682a924d620ad9fa5a1bc
Sahil Gulihar <sahilgullihar@gmail.com>
2026-06-05 22:18:47 +0530

    minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,30 +55,6 @@ export default function Page() {
         <WorkExperience />
       </section>
 
-      <section id="education">
-        <div className="flex min-h-0 flex-col gap-y-3">
-          <BlurFade delay={BLUR_FADE_DELAY * 7}>
-            <h2 className="text-xl font-bold">Education</h2>
-          </BlurFade>
-          {DATA.education.map((education, id) => (
-            <BlurFade
-              key={education.school}
-              delay={BLUR_FADE_DELAY * 8 + id * 0.05}
-            >
-              <ResumeCard
-                key={education.school}
-                href={education.href}
-                logoUrl={education.logoUrl}
-                altText={education.school}
-                title={education.school}
-                subtitle={education.degree}
-                period={`${education.start} - ${education.end}`}
-              />
-            </BlurFade>
-          ))}
-        </div>
-      </section>
-
       <section id="projects">
         <div className="w-full space-y-8">
           <BlurFade delay={BLUR_FADE_DELAY * 11}>
